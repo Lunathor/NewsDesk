04201d379cb36546b6886fe11dee5b835e43cee0
Lunathor <mrcoopgmz@gmail.com>
2024-09-30 15:59:11 +0300

    Django settings commit

--- NewsDesk/desk/models.py
+++ NewsDesk/desk/models.py
@@ -1,1 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    pass
+
+
+class Image(models.Model):
+    pass
+
+
+class Video(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
