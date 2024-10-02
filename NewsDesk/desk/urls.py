84a0ec5612e12e4c5ac33c6ca5d6d2bb5a1a5319
Lunathor <mrcoopgmz@gmail.com>
2024-10-02 13:58:32 +0300

    Представления на редакт и удаление

--- NewsDesk/desk/urls.py
+++ NewsDesk/desk/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('post/create/', PostCreateView.as_view(), name='post_create'),
     path('post/update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
     path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
+    path('delete-image/<int:pk>/', delete_image, name='delete_image'),
+    path('delete-video/<int:pk>/', delete_video, name='delete_video'),
 ]
